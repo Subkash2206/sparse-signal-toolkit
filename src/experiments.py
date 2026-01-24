192a69869e39f058c877987a40d8c659c4f07a51
Subhash Kashyap <subhashkashyap2206@gmail.com>
2026-01-24 19:20:23 +0530

    Demonstrate aliasing via undersampling (fs = 180 Hz)

--- src/experiments.py
+++ src/experiments.py
@@ -1,16 +1,17 @@
 # src/experiments.py
 
 import numpy as np
-#using matplot to plot the experimental results
+# using matplotlib to plot the experimental results
 import matplotlib.pyplot as plt
 
-#importing the two tone summation of 2 sine waves
+# importing the two tone summation of 2 sine waves
 from signals import two_tone_signal
-#importing the mathematical implementation of DFT
+# importing the mathematical implementation of DFT
 from dft import dft
 
-#defines sampling frequency and observation window
-fs = 1000
+# defines sampling frequency and observation window
+# NOTE: fs intentionally violates Nyquist for 120 Hz
+fs = 180
 duration = 1.0
 
 t, x = two_tone_signal(50, 120, fs, duration)
@@ -18,23 +19,22 @@ t, x = two_tone_signal(50, 120, fs, duration)
 # we convert time domain to frequency domain
 X = dft(x)
 
-
 # this tells us where each Fourier bin lives in Hz
 freqs = np.arange(len(X)) * fs / len(X)
 
-#plotting
+# plotting time-domain signal
 plt.figure()
 plt.plot(t, x)
 plt.xlabel("Time (s)")
 plt.ylabel("Amplitude")
-plt.title("Two-Tone Signal (50 Hz + 120 Hz)")
+plt.title("Two-Tone Signal (Aliasing Case, fs = 180 Hz)")
 plt.show()
 
-
+# plotting frequency-domain magnitude spectrum
 plt.figure()
 plt.stem(freqs, np.abs(X) / len(X))
-plt.xlim(0, 200)
+plt.xlim(0, fs / 2)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Magnitude")
-plt.title("DFT Magnitude Spectrum")
+plt.title("Aliasing in Frequency Domain (fs = 180 Hz)")
 plt.show()
